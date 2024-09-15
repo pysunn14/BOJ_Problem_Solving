a8876d468bcc4db241968795ef1057f126289f5c
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2024-09-16 02:00:40 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,35 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ub upper_bound
+#define lb lower_bound
+#define pii pair<int, int>
+#define test(a) \
+    int _;      \
+    cin >> _;   \
+    FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio()                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define weight first
+#define vertex second
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+    int N; cin>>N;
+    if(N == 1) cout<<"B";
+    else cout<<"A";
+};
