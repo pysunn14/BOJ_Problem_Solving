7351312fd023093fdeaa1908af22a14a375aa90b
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2024-10-17 23:37:13 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c; cin >> c;
+    switch (c)
+    {
+        case 'M':
+        cout << "MatKor";
+        break;
+    
+        case 'W':
+        cout << "WiCys";
+        break;
+
+        case 'C':
+        cout << "CyKor";
+        break;
+
+        case 'A':
+        cout << "AlKor";
+        break;
+
+        case '$':
+        cout << "$clear";
+        break;
+    }
+}
