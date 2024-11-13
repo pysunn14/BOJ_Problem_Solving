5aae8cca332706aa72aacd1f054e0ff63600d050
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2024-11-14 00:05:52 +0900

    [Bronze V] Title: CAPS, Time: 20 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,25 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    string s; cin >> s;
+
+    FOR(i,s.size(),0)
+    {
+        cout << char(s[i] - 'a' + 'A');
+    }
+}
