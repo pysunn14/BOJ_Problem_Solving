c3a86633123c792959b573f5034b43c10b736495
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2024-12-18 21:56:30 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
@@ -1,2 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std; int main(int n){cin >> n;int ans[8]={2,1,2,3,4,5,4,3};cout << ans[n%8];}
+#include <iostream>
+main(int n){std::cin>>n;int ans[8]={2,1,2,3,4,5,4,3};std::cout<<ans[n%8];}
