5361e8a9c808625229aeb6b507e87b493e62c8d5
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2024-12-20 10:33:09 +0900

    [Bronze III] Title: 앵그리 창영, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.cc
@@ -0,0 +1,28 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int N,W,H; cin >> N >> W >> H;
+
+    FOR(i,N,0)
+    {
+        int x; cin >> x;
+
+        if(x <= sqrt(W*W+H*H)) cout << "DA\n";
+        else cout << "NE\n";
+    }
+}
