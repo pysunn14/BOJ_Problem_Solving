3eccfd121a12d318f255ac4d9b1087cbf905b472
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-02-18 17:00:52 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4212 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int n; cin >> n;
+
+    vector<int> v(n);
+
+    FOR(i,n,0) cin >> v[i];
+
+    sort(all(v));
+
+    FORE(vv,v) cout << vv << '\n';
+}
