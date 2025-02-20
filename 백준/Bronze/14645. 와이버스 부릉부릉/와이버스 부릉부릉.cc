18cfac34faff3f8dc2ff79c3c8621d09bef25c4b
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-02-20 13:27:53 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int n,k; cin >> n >> k;
+    FOR(i,n,0){int a,b; cin >> a >> b;}
+    cout << "비와이";
+}
