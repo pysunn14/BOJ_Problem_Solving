4098451eb0a935e20aeb7aee5a7912b8c074c8d6
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-04-03 23:05:38 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int a,b,c; cin >> a >> b >> c;
+
+    if(a == 60 && b == 60 && c == 60) cout << "Equilateral";
+
+    else if(a + b + c == 180 && (a == b || b == c || c == a)) cout << "Isosceles";
+
+    else if(a + b + c == 180) cout << "Scalene";
+
+    else cout << "Error";
+
+}
