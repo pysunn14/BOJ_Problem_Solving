27a4f892bff77bf66ffe44e208a38d22471db607
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-04-09 03:08:59 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    test(t)
+    {
+        int a,b; cin >> a >> b;
+        cout << "Case" << ' ' <<t+1<< ": " << a + b << '\n';
+    }
+}
