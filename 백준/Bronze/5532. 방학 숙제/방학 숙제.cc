815e355d9e6613ed9286d79460051d3c2ea908c6
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-04-16 00:45:04 +0900

    [Bronze IV] Title: 방학 숙제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int l,a,b,c,d;
+    cin >> l >> a >> b >> c >> d;
+
+    cout << l - max((a + c - 1 )/c, (b + d - 1)/d);
+    
+}
