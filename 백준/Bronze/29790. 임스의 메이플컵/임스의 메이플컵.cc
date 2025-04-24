a31152a484cc91b0f918908e90a320772c115944
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-04-25 00:30:06 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int N,U,L; cin >> N >> U >> L;
+
+    if(N >= 1000)
+    {
+        if(U >= 8000 || L >= 260) cout << "Very Good";
+        else cout << "Good";
+    }
+
+    else cout << "Bad";
+}
