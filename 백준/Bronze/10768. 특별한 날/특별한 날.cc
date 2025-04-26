f77c9d435ffdcb12ee0bfdcb733b05b5733ca48a
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2025-04-26 13:11:03 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int m;
+    int d;
+    cin >> m >> d;
+
+    if(m > 2) cout << "After";
+    else if(m == 2)
+    {
+        if(d > 18) cout << "After";
+        else if(d == 18) cout << "Special";
+        else cout << "Before";
+    }
+    else cout << "Before";
+}
