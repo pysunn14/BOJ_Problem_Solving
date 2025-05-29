1f3a4cd43ef01a3cb6bfd4caf53900f519118bb1
Minseok Kim <161586699+pysunn14@users.noreply.github.com>
2025-05-30 00:16:06 +0900

    [Bronze III] Title: 모험의 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31789. 모험의 시작/모험의 시작.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    int n; cin >> n;
+
+    int x,s; cin >> x >> s;
+
+    FOR(i,n,0)
+    {
+        int c,p; cin >> c >> p;
+        
+        if(c <= x && p > s)
+        {
+            cout << "YES";
+            exit(0);
+        } 
+    }
+
+    cout << "NO";
+}
