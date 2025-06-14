cc6ceee28b660ce19d0ca22590530b5c545ac763
Minseok Kim <161586699+pysunn14@users.noreply.github.com>
2025-06-15 01:27:34 +0900

    [Bronze III] Title: 2의 제곱인가?, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/11966. 2의 제곱인가？/2의 제곱인가？.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define test(a) int _; cin >> _; FOR(a, _, 0)
+typedef long long ll;
+#define FOR(a, b, c) for (int a = c; a < b; a++)
+#define FORE(a, b) for (const auto &a : b)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define mid (lo + hi) / 2
+using namespace std;
+
+int main()
+{
+    fio();
+
+    ll N; cin >>  N;
+
+    bool ok = true;
+
+    if(log2(N) != int(log2(N))) ok = false;
+    
+    cout << ok;
+}
