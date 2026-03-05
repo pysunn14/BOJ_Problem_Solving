fcf98f492e9967caf210a48cb44b503480512306
Minseok Kim <161586699+pysunn14@users.noreply.github.com>
2026-03-05 22:39:51 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -0,0 +1,6 @@
+select U.USER_ID, U.NICKNAME, sum(B.PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD B join USED_GOODS_USER U on B.WRITER_ID = U.USER_ID 
+where B.STATUS = 'DONE'
+group by B.WRITER_ID 
+having TOTAL_SALES >= 700000
+order by TOTAL_SALES asc 
